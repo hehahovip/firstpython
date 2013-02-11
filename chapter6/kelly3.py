189c36447da88ad5f30d5ea82a394deba803df60
Kevin Wang <yingkang.wang@hp.com>
2013-02-11 16:18:48 +0800

    add web app

--- chapter6/kelly3.py
+++ chapter6/kelly3.py
@@ -2,9 +2,6 @@
 # use for loop to convert the data
 # use not in for the list
 
-
-
-
 def readdatainfile(filename):
 	with open(filename) as filedata:
 		return filedata.readline()
@@ -25,6 +22,38 @@ def sanitize(strdata):
 
 	return minute + ':' + secode
 
+def top3times(newlist):
+	uniquelist = []
+	for each_item in newlist:
+		if each_item not in uniquelist:
+			uniquelist.append(each_item)
+
+	return uniquelist
+
+def parsedata(data):
+	persondata = {}
+	persondata['name'] = data.pop(0)
+	persondata['birthdate'] = data.pop(0)
+	newlist = [sanitize(item) for item in data]
+	persondata['times'] = newlist
+	return persondata
+
+def createAthlete(persondata):
+	a = Athlete(persondata['name'], persondata['birthdate'], persondata['times'])
+	return a
+
+
+class Athlete(object):
+	"""docstring for Athlete"""
+	def __init__(self, name, birthdate='',times=[]):
+		super(Athlete, self).__init__()
+		self.name = name
+		self.birthdate = birthdate
+		self.times = times
+
+	def top3(self):
+		return top3times(self.times)
+
 
 file_james = "james2.txt"
 file_julie = "julie2.txt"
@@ -35,22 +64,10 @@ filedata = readdatainfile(file_james)
 
 data = filedata.strip().split(',')
 
-print("data: ", data)
-name = data.pop(0)
-birthdate = data.pop(0)
-print("name: " + name + ", birthdate: " + birthdate)
-print("poped data: ", data)
-newlist = [sanitize(item) for item in data]
+persondata = parsedata(data)
 
-print("newlist", newlist)
+person = createAthlete(persondata)
 
-print(newlist)
-print(sorted(newlist))
-
-uniquelist = []
-for each_item in newlist:
-	if each_item not in uniquelist:
-		uniquelist.append(each_item)
-
-
-print(uniquelist[0:3])
+print("name: " + person.name + ", birthdate: " + person.birthdate)
+print("poped data: ", person.times)
+print("top 3 times", person.top3())
